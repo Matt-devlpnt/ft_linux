d3a2f1c2bc46d0f05d2c98db24ad1638dde5645c
Matthieu Cordes <mcordes@c4r1p6.42nice.fr>
2026-01-22 16:32:33 +0100

    -

--- script_1.sh
+++ script_1.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-./configure --prefix=/usr                           \
-            --docdir=/usr/share/doc/procps-ng-4.0.5 \
-            --disable-static                        \
-            --disable-kill                          \
-            --enable-watch8bit                      \
-            --with-systemd
+./configure --bindir=/usr/bin     \
+            --libdir=/usr/lib     \
+            --runstatedir=/run    \
+            --sbindir=/usr/sbin   \
+            --disable-chfn-chsh   \
+            --disable-login       \
+            --disable-nologin     \
+            --disable-su          \
+            --disable-setpriv     \
+            --disable-runuser     \
+            --disable-pylibmount  \
+            --disable-liblastlog2 \
+            --disable-static      \
+            --without-python      \
+            ADJTIME_PATH=/var/lib/hwclock/adjtime \
+            --docdir=/usr/share/doc/util-linux-2.41.1
